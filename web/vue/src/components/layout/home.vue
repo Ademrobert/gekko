f1fe51f4af00389ec5c26e28b62895c4a3cedcd4
Ademrobert <adembenothman@gmail.com>
2021-12-26 04:49:40 -0800

    first push

---
+++ web/vue/src/components/layout/home.vue
@@ -0,0 +1,33 @@
+<template lang='pug'>
+  section.contain.grd-row
+    .grd-row-col-3-6(v-html='left')
+    .grd-row-col-3-6.txt--center
+      img(src='static/gekko.jpg')
+      p
+        em The most valuable commodity I know of is information.
+</template>
+
+<script>
+import marked from '../../tools/marked';
+
+const left = marked(`
+
+## Crypto Gram ##
+
+Crypto Gram is a Bitcoin trading bot and backtesting platform that
+connects to popular Bitcoin exchanges. It is written in javascript
+and runs on nodejs.
+
+[Find out more](in server Files! C:\Users\Administrator\Desktop\gekko ).
+
+
+`);
+
+export default {
+  data: () => {
+    return {
+      left
+    }
+  }
+}
+</script>
